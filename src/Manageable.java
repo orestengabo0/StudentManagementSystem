d53ebcb8c7c046f51b5a9772c44df97785878e40
orestengabo0 <orestengabo0@gmail.com>
2024-10-24 20:50:03 +0200

    adding Manageable interface

--- src/Manageable.java
+++ src/Manageable.java
@@ -1,2 +1,6 @@
 public interface Manageable {
+    void addStudent(Student student);
+    Student findStudent(String studentId);
+    void updateStudent(String studentId, String studentName, String studentAge, String studentClass);
+    void deleteStudent(String studentId);
 }
